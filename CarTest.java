601230fd66b46272a6b2ecc876505b375137fb43
marabou03 <t.martin.felix@gmail.com>
2024-01-23 14:36:16 +0100

    hello2

--- CarTest.java
+++ CarTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-
+    // HELLO!
     private static Car volvo;
     private static Car saab;
     @BeforeAll
