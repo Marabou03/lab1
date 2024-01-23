9acd2e01a9ac2e0b6256682ceec9d37ed0daeac9
marabou03 <t.martin.felix@gmail.com>
2024-01-23 14:33:12 +0100

    hello2

--- CarTest.java
+++ CarTest.java
@@ -1,23 +1,40 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
+    private static Car volvo;
+    private static Car saab;
+    @BeforeAll
+    public static void init() {
+        volvo  = new Volvo240();
+        saab = new Saab95();
+    }
+
     @Test
     void move() {
+        volvo.startEngine();
+        volvo.move();
+        assertEquals(0.1, volvo.point.getX());
     }
 
     @Test
     void turnLeft() {
+        saab.turnLeft();
+        assertEquals(3, saab.direction);
     }
 
     @Test
     void turnRight() {
+        volvo.turnRight();
+        assertEquals(1, volvo.direction);
     }
 
     @Test
     void getNrDoors() {
+
     }
 
     @Test
