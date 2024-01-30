a49c5adfb88badcaef1ed7446e9a57218b0d2a34
lordmin33 <albin.karlsteen@hotmail.com>
2024-01-30 13:41:53 +0100

    Das passt wie die Faust aufs Auge. |Sieben|

--- CarTest.java
+++ CarTest.java
@@ -4,41 +4,25 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.Test;
 
-
 import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CarTest {
-    private static Volvo240 volvo;
-    private static Volvo240 volvo2;
-    private static Volvo240 volvo3;
-    private static Saab95 saab;
-    private static Saab95 saab2;
+    private static Car volvo;
+    private static Car volvo2;
+    private static Car saab;
     private static Scania scania;
     private static Truck truck;
-    private static Truck truck2;
-    private static Truck truck3;
-    private static Truck truck4;
-    private static Workshop<Volvo240> vShop;
-
-
     @BeforeAll
     public static void init() {
-        volvo = new Volvo240();
-        volvo2 = new Volvo240();
-        volvo3 = new Volvo240();
+        volvo  = new Volvo240();
+        volvo2  = new Volvo240();
         saab = new Saab95();
-        saab2 = new Saab95();
         scania = new Scania();
         truck = new VolvoTruck();
-        truck2 = new VolvoTruck();
-        truck3 = new VolvoTruck();
-        truck4 = new VolvoTruck();
-        vShop = new Workshop<>(10, "Car Workshop", Volvo240.class);
     }
 
     @Test @Order(1)
@@ -61,7 +45,7 @@ class CarTest {
     }
 
 
-    @Test @Order(3)
+    @Test@Order(3)
     void turnLeft() {
         volvo.turnLeft();
         assertEquals(Car.Direction.NORTH, volvo.direction);
@@ -134,6 +118,8 @@ class CarTest {
         volvo.incrementSpeed(10);
         saab.startEngine();
         saab.incrementSpeed(10);
+        int initialSpeed = 0;
+        int amount = 10;
         assertTrue(volvo.getCurrentSpeed() > 10);
         assertTrue(saab.getCurrentSpeed() > 10);
     }
@@ -155,7 +141,7 @@ class CarTest {
         double initialSpeed = 0.1;
         volvo.gas(1);
         saab.gas(1);
-        assertTrue(volvo.getCurrentSpeed() > initialSpeed);
+        assertTrue( volvo.getCurrentSpeed() > initialSpeed);
         assertTrue(saab.getCurrentSpeed() > initialSpeed);
 
         volvo.brake(1);
@@ -184,20 +170,12 @@ class CarTest {
     }
 
     @Test @Order(8)
-    void raiseFlak() {
-        scania.stopEngine();
+    void raiseFlak(){
         scania.raiseFlak(2);
         assertEquals(2, scania.getFlakAngle());
     }
-
-    @Test @Order(9)
-    void startEngineScania() {
-        scania.startEngine();
-        assertEquals(0.0, scania.getCurrentSpeed());
-    }
-
     @Test @Order(10)
-    void lowerFlak() {
+    void lowerFlak(){
         scania.lowerFlak(2);
         assertEquals(0, scania.getFlakAngle());
         scania.lowerFlak(2);
@@ -206,121 +184,63 @@ class CarTest {
         assertEquals(0, scania.getFlakAngle());
     }
 
+    @Test @Order(9)
+    void startEngineScania(){
+    scania.startEngine();
+    assertEquals(0.0, scania.getCurrentSpeed());
+
+    }
+
     @Test @Order(11)
-    void startEngineScania2() {
-        scania.startEngine();
-        assertEquals(0.1, scania.getCurrentSpeed());
+    void startEngineScania2(){
+    scania.startEngine();
+    assertEquals(0.1, scania.getCurrentSpeed());
 
     }
 
     @Test @Order(12)
-    void getFlakAngle() {
-        assertEquals(0, scania.getFlakAngle());
-        scania.raiseFlak(2);
-        assertEquals(0, scania.getFlakAngle());
-        scania.lowerFlak(2);
-        assertEquals(0, scania.getFlakAngle());
-        scania.stopEngine();
-        scania.raiseFlak(2);
-        assertEquals(2, scania.getFlakAngle());
+    void getFlakAngle(){
+    assertEquals(0, scania.getFlakAngle());
+    scania.raiseFlak(2);
+    assertEquals(0, scania.getFlakAngle());
+    scania.lowerFlak(2);
+    assertEquals(0, scania.getFlakAngle());
+    scania.stopEngine();
+    scania.raiseFlak(2);
+    assertEquals(2, scania.getFlakAngle());
     }
 
     @Test @Order(12)
-    void rampUpDown() {
+    void rampUpDown(){
         truck.stopEngine();
         assertEquals(0, truck.getCurrentSpeed());
         truck.lowerRamp();
-        assertFalse(truck.getRampUp());
+        assertEquals(false, truck.getRampUp());
         truck.raiseRamp();
-        assertTrue(truck.getRampUp());
+        assertEquals(true, truck.getRampUp());
 
     }
 
     @Test @Order(13)
-    void loadCar() {
-        truck.lowerRamp();
-        volvo2.point.setLocation(2, 2);
-        truck.loadCar(volvo2);
-        assertEquals(truck.point.getX(), volvo2.point.getX());
-        assertEquals(truck.point.getY(), volvo2.point.getY());
-        assertEquals(truck.getPoint().getY(), volvo2.getPoint().getY());
-        assertEquals(1, truck.getLoadedCars().size());
+    void l(){
+
     }
-
-
     @Test @Order(14)
-    void unload() {
+    void loadCar(){
         truck.lowerRamp();
-        truck.loadCar(volvo3);
-        truck.unloadCar();
-        assertEquals(5, volvo3.point.getX());
-        assertEquals(5, volvo3.point.getY());
-        assertEquals(0, truck.point.getX());
-        assertEquals(0, truck.point.getY());
-    }
+        volvo2.point.setLocation(2,2);
+        truck.loadCar(volvo2);
+        assertEquals(truck.getPoint().getX(), volvo2.getPoint().getX());
+        assertEquals(truck.getPoint().getY(), volvo2.getPoint().getY());
 
+    }
     @Test @Order(15)
-    void MoveTruck() {
-        truck.raiseRamp();
-        truck.startEngine();
-        truck.move();
-        assertEquals(0.1, truck.point.getY());
-        assertEquals(0, truck.point.getX());
-        truck.stopEngine();
+    void unload(){
         truck.lowerRamp();
-        truck.loadCar(volvo);
-        truck.raiseRamp();
-        truck.startEngine();
-        truck.move();
-        assertEquals(0.2, truck.point.getY());
-        assertEquals(0, truck.point.getX());
-        assertEquals(0.2, volvo.point.getY());
-        assertEquals(0, volvo.point.getX());
+        volvo2.point.setLocation(2,2);
+        truck.loadCar(volvo2);
+        assertEquals(truck.getPoint().getX(), volvo2.getPoint().getX());
+        assertEquals(truck.getPoint().getY(), volvo2.getPoint().getY());
 
-    }
-
-
-    @Test @Order(16)
-    void testing() {
-        vShop.point.setLocation(6, 6);
-        assertEquals(6, vShop.getPoint().getX());
-        assertEquals(6, vShop.getPoint().getY());
-    }
-
-    @Test @Order(17)
-    void typeCarAllowed() {
-        volvo2.point.setLocation(7, 7);
-        vShop.typeCarAllowed(volvo2);
-        assertEquals(vShop.getPoint().getX(), volvo2.getPoint().getX());
-        assertEquals(vShop.getPoint().getY(), volvo2.getPoint().getY());
-        saab2.point.setLocation(6, 6);
-    }
-
-    @Test @Order(20)
-    void loadWrongCarInWorkshop(){
-        volvo2.point.setLocation(5,5);
-        vShop.typeCarAllowed(volvo2);
-        assertEquals(2, vShop.currentCars.size());
-        //vShop.typeCarAllowed(saab2); // gives a static (compile time) error.
-
-        /*Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            vShop.typeCarAllowed(saab2);});
-        System.out.println(exception);*/
-    }
-    @Test @Order(19)
-    void loadTruckWithTruck() {
-        try {
-            truck3.lowerRamp();
-            truck3.loadCar(truck4);
-        } catch (Exception e) {
-            System.out.println("Caught exception: " + e.getMessage());
-        }
-    }
-    @Test @Order(21)
-    void unlaodWorkkShop(){
-
-        assertEquals(2, vShop.getLoadedCar().size());
-        vShop.theCarYouGet();
-        assertEquals(1, vShop.getLoadedCar().size());
     }
 }
--- Truck.java
+++ Truck.java
@@ -4,92 +4,70 @@ import java.util.List;
 import java.lang.Math;
 
 
-public class Truck extends Car {
-    private final Loading loader;
+public abstract class Truck extends Car {
     private final int maxCars; //Max amount of cars possible to carry.
     private final List<Car> loadedCars; // the amount of cars being carried.
     private boolean rampUp; // Initial state is ramp up/closed.
-
-
-    public Truck(int nrDoors, double enginePower, Color color, String modelName, int maxCars, String vehicleType) {
-        super(nrDoors, enginePower, color, modelName, vehicleType);
-        this.loader = new Loading();
-        this.vehicleType = vehicleType;
+    public Truck(int nrDoors, double enginePower, Color color, String modelName, int maxCars) {
+        super(nrDoors, enginePower, color, modelName);
         this.maxCars = maxCars;
         this.loadedCars = new ArrayList<>(maxCars);
         this.rampUp = true;
-
     }
-    protected boolean getRampUp(){
+    public boolean getRampUp(){
         return rampUp;
 
     }
 
-    protected void lowerRamp() {
+    public void lowerRamp() {
         if (currentSpeed == 0) {
             rampUp = false;
         }
     }
 
-    protected void raiseRamp() {
+    public void raiseRamp() {
         rampUp = true;
     }
 
-    protected void loadCar(Car car) {
-        //loader.loadCar(this, car);
-        /*System.out.println(this.carType);
-        System.out.println(car.carType);*/ // to see what happens when using loadcar
-        boolean allowed = car.vehicleType.equals(this.vehicleType);
-        if (!allowed){
-            if(!rampUp && (loadedCars.size() < maxCars)){
-                double distance = loader.calculateDistance(car.getPoint(), this.getPoint());
-                if (distance < 5){
-                    loadedCars.add(car);
-                    car.point = this.getPoint();
-                } else{
-                    throw new IllegalArgumentException("Car is too far away");
-                }
-            }else {
-                throw new IllegalArgumentException("Either the truck is full or the ramp is up");
+    public void loadCar(Car car) {// car be in a certain range like radius=10, can't be another truck //(car != Truck)
+        if(!rampUp && (loadedCars.size() < maxCars)){
+            Point carPos = car.getPoint();
+            Point truckPos = this.getPoint(); // needs to be fixed
+            double posX = Math.pow(carPos.getX()-truckPos.getX(), 2);
+            double posY = Math.pow(carPos.getY()-truckPos.getY(), 2);
+            double distance = Math.sqrt(posX+posY);
+            if (distance < 5){
+                loadedCars.add(car);
+                //car.getPoint().setLocation(this.getPoint().getX(), this.getPoint().getY()); // does it set the cordenates of the truck?
+                car.point = this.getPoint();
+            } else{
+                throw new IllegalArgumentException("Car is too far away");
             }
-        } else {
-            throw new IllegalArgumentException("can't load a truck in a truck");
+        }else {
+            throw new IllegalArgumentException("Either the truck is full or the ramp is up");
         }
     }
 
-    protected void unloadCar() {
-        if(!rampUp && !loadedCars.isEmpty() ){
-            Car lastCar = loadedCars.getLast();
-            Point carPoint = new Point(lastCar.point.getX(), lastCar.point.getY());// new Point
-            carPoint.setLocation(point.getX() + 5,point.getY() + 5);
-            lastCar.point = carPoint;
-            loadedCars.removeLast();
+    public void unloadCar() {
+        if(!rampUp && !loadedCars.isEmpty()){
+            int i = loadedCars.size() - 1;
+            Car lastCar = loadedCars.get(i);
+            loadedCars.remove(i);
+            lastCar.point.setLocation(point.getX() + 1,point.getY() + 1); // does it set the cordenates of the car to be next the truck?
         }else{
             throw new IllegalArgumentException("Truck is empty or the ramp is up");
         }
+
     }
 
-    protected List<Car> getLoadedCars() {
+    public List<Car> getLoadedCars() {
         return new ArrayList<>(loadedCars);
     }
 
     @Override
     protected void startEngine() {
-        if (rampUp) {
+        if (rampUp == true) {
             super.startEngine();
         }
-    }
-    @Override
-    public void move() {
-        super.move();
-        if(!loadedCars.isEmpty()){
-            for (Car car : loadedCars){
-                car.point = this.getPoint();
-            }
-        }
-    }
-
-    public int getMaxCars() {
-        return maxCars;
     }
 }
